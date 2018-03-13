ec78bf3af5de98b5e7923079c9043fa087f6f676
Giorgi Koguashvili <kogo27@gmail.com>
2018-03-13 17:09:43 +0400

    added simple example of Either monad

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Types.Union;
+
+namespace Types
+{
+    public class Code { }
+    public class CompileError { }
+    public class CompiledCode : Either<Code, CompileError>
+    {
+        public CompiledCode(string codeTemplate) : base(() =>
+        {
+            return new CompiledCode(new Code());
+        }) {}
+
+        protected CompiledCode(Code t1) : base(t1)
+        {
+        }
+
+        protected CompiledCode(CompileError t2) : base(t2)
+        {
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = new CompiledCode("asdf")
+                .Match(
+                    (cc) => cc.ToString(),
+                    (e) => e.ToString()
+                );
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Union/Union2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Types.Union
+{
+    public abstract class Union<T1, T2> 
+           where T1 : class
+           where T2 : class
+    {
+        private readonly T1 _t1;
+        private readonly T2 _t2;
+        private object v;
+
+        protected Union(T1 t1) { _t1 = t1; }
+        protected Union(T2 t2) { _t2 = t2; }
+
+        protected Union(object v)
+        {
+            this.v = v;
+        }
+
+        public TResult Match<TResult>(
+            Func<T1, TResult> f1,
+            Func<T2, TResult> f2
+        )
+        {
+            if (_t1 != null)
+            {
+                return f1(_t1);
+            }
+            else if (_t2 != null)
+            {
+                return f2(_t2);
+            }
+            throw new Exception("can't match");
+        }
+
+        //public object Content()
+        //{
+        //    if (_t1 != null)
+        //    {
+        //        return _t1;
+        //    }
+        //    return _t2;
+        //}
+    }
+}
